71fd82d5e3a266e35ba0a4027c3ef3ba583dc4d1
Raihan Saputra <rraihansaputra@users.noreply.github.com>
2019-09-18 22:41:15 +0700

    Initializing postgres and SQLAlchemy (#9)
    * Add and configure PostgreSQL, Added the User model, Added dotenv to work
    with .env configuration
    
    * Resolve PR comments
    
    - Removed python-dotenv and flask-script
    - Removed manage.py (migrations are handled directly by flask-migrate)
    - Pinned versions on Pipfile
    - Removed .env and added .env.example
    - config.py - changes to direct configs to .env variables
    - app.py - changes to accomodate flask-migrate
    
    * added .env to .gitignore
    
    * Updated README for PostgreSQL version requirement
    
    * Updated README.md to use flask-migrate instead of flask-script
    
    * .env delete.. somehow it didn't work in the merge

--- server/app.py
+++ server/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
-
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+from models import User
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-

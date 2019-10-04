eaaddc53aee7d95791bdb515abd0a90272ac253e
Raihan Saputra <rraihansaputra@gmail.com>
2019-10-04 19:59:01 +0700

    initial efforts to upload to S3; still struggling with ACL permissions

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -30,6 +30,61 @@ const img = {
   height: "100%"
 };
 
+
+// TODO: move function to api file
+async function get_presigned_post(filename, filetype) {
+
+  // TODO: change to appropriate values
+  const token = "" // Token from login to get the presigned post headers
+  var url = new URL("http://127.0.0.1:5000/sign_s3")
+
+  const params = {
+    file_name: filename,
+    file_type: filetype,
+  }
+  url.search = new URLSearchParams(params)
+
+  return fetch(url , {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + token,
+    },
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`)
+    }
+    return response.json();
+  }).then(body => {
+    return(body)
+  })
+}
+
+async function upload_to_s3(file) {
+  const presigned_post = await file.presigned_post
+  const url = await presigned_post.url
+  console.log(presigned_post.data)
+  console.log(presigned_post.data.fields)
+  var headers = await presigned_post.data.fields
+
+  const options = {
+    method: 'PUT',
+    headers,
+    body: file,
+  }
+
+  return fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        // TODO: handle errors from react's side
+        throw new Error(`${response.status}: ${response.statusText}`)
+      }
+      return url;
+    })
+}
+
 export default function ImageUpload(props) {
   const [files, setFiles] = useState([]);
   const { getRootProps, getInputProps } = useDropzone({
@@ -42,6 +97,13 @@ export default function ImageUpload(props) {
           })
         )
       );
+      acceptedFiles.map(file => {
+        const presigned_post = get_presigned_post(file.name,file.type);
+        Object.assign(file, {
+          presigned_post,
+        })
+        upload_to_s3(file)
+      });
     }
   });
 
